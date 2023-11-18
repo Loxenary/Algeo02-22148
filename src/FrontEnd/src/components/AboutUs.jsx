202bdd3aca729fdf6f99dcab0904393bbc3b4259
Loxenary <110291644+Loxenary@users.noreply.github.com>
2023-11-18 21:12:51 +0700

    build(Structure) : final folder structure

--- src/FrontEnd/src/components/AboutUs.jsx
+++ src/FrontEnd/src/components/AboutUs.jsx
@@ -11,8 +11,8 @@ const AboutUs = () => {
           <div className='w-full bg-white shadow-xl flex flex-col p-4 my-4 rounded-lg h-[400px] hover:scale-105 duration-300'>
               <img src={Davis} alt="" className='rounded-full border-8 border-white mx-auto mt-[-5rem] object-cover w-[200px] h-[200px]'/> 
               <h2 className='text-2xl font-bold text-center rounded-full shadow-md bg-yellow-100 py-3 my-3'>Davis</h2>
-              <p className='text-center py-10'>
-                Drop the image dataset in the form of a folder containing a collection of images. This image dataset is needed before the searching process so that there is a comparison for the image you want to search for.
+              <p className='text-center pb-10 pt-2'>
+              A sophomore IT student at ITB with the id of 13522157. <br></br>currently living in Bandung. I have a wide range of interests, including Web Development, Game Development, Mobile Applications, and Cybersecurity. Currently, the main focus is on mastering Game Development and refining skills in Web Development.
               </p>
           </div>
           <div className='w-full bg-white shadow-xl flex flex-col p-4 my-4 rounded-lg h-[400px] hover:scale-105 duration-300'>
