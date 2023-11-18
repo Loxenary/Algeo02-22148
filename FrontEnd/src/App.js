2089716cd37b976a419075bcc731aa6c9394e769
Loxenary <110291644+Loxenary@users.noreply.github.com>
2023-11-18 08:08:05 +0700

    fix : Buf-fix

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -4,10 +4,6 @@ import Navbar from './components/Navbar';
 import Reverse from './components/Reverse';
 import HowToUse from './components/HowToUse';
 import AboutUs from './components/AboutUs';
-import Pagination from './components/Pagination';
-import Page from './components/Page';
-import Camera from './components/Camera';
-import DatasetUpload from './components/DatasetUpload';
 import Information from './components/Information';
 
 
